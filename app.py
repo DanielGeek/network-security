d3183597e713347d73ddc0178c03c4f7c5e9efe3
Daniel Ángel Barreto <salomons.cripto@gmail.com>
2026-09-13 13:54:46 -0400

    feat: add predict route

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ ca = certifi.where()
 from dotenv import load_dotenv
 load_dotenv()
 MONGO_DB_URL = os.getenv("MONGODB_URI")
-print(MONGO_DB_URL)
+# print(MONGO_DB_URL)
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from networksecurity.exception.exception import NetworkSecurityException
@@ -16,6 +16,7 @@ from networksecurity.pipeline.training_pipeline import TrainingPipeline
 
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.templating import Jinja2Templates
 from uvicorn import run as app_run
 from fastapi.responses import Response
 from starlette.responses import RedirectResponse
@@ -43,6 +44,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+templates = Jinja2Templates(directory="./templates")
+
 @app.get("/", tags=["authentication"])
 async def index():
     return RedirectResponse(url="/docs")
@@ -55,6 +58,29 @@ async def train_route():
         return Response("Training is successful")
     except Exception as e:
         raise NetworkSecurityException(e, sys)
+
+@app.post("/predict")
+async def predict_route(request: Request, file: UploadFile = File(...)):
+    try:
+        df = pd.read_csv(file.file)
+        #print(df)
+        preprocesor = load_object("final_model/preprocessor.pkl")
+        final_model = load_object("final_model/model.pkl")
+        network_model = NetworkModel(preprocessor = preprocesor, model = final_model)
+        print(df.iloc[0])
+        y_pred = network_model.predict(df)
+        print(y_pred)
+        df['predicted_column'] = y_pred
+        print(df['predicted_column'])
+        #df['predicted_column'].replace(-1, 0)
+        #return df.to_json()
+        df.to_csv('prediction_output/output.csv')
+        table_html = df.to_html(classes='table table-striped')
+        #print(table_html)
+        return templates.TemplateResponse(request = request, name = "table.html", context = {"table": table_html})
+
+    except Exception as e:
+            raise NetworkSecurityException(e,sys)
 
 if __name__ == "__main__":
     app_run(app, host="0.0.0.0", port = 8000)
